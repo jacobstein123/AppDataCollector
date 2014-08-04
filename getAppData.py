ab48381538a91880a43f5070a912cc235078d33a
jacobstein123 <jacobstein123@gmail.com>
2014-08-04 19:12:21 -0400

    Added download error handling, and fixed signature grab for large files

--- getAppData.py
+++ getAppData.py
@@ -4,11 +4,15 @@ import zipfile
 import binascii
 import BeautifulSoup
 import urllib2
+import os
 
 def download_apk(package_name, location): #C:\SSD-Folders\Google Drive (SSD)\Projects\AppDataGrabber\Testing\APKs
     """
     downloads the apk file to the specified folder
     """
+    error_text = ['''Ops, we are not able to download. (invalid package/non-free/not compatible).''',
+                  '''Ops, we are out of quota, please try others apps or try again in another hour''',
+                  '''Please make sure package name or URL is valid''']
     pref = {"browser.helperApps.neverAsk.saveToDisk": "application/vnd.android.package-archive",
                 "browser.download.folderList": 2,
                 "browser.download.dir": location}
@@ -17,8 +21,25 @@ def download_apk(package_name, location): #C:\SSD-Folders\Google Drive (SSD)\Pro
     br.find_by_id('packagename').first.fill(package_name)
     br.find_by_id('download_apk').first.click()
     down_button = br.find_by_id('download_apk_link').first
+    for i in range(60):
+        if down_button.visible:
+            down_button.click()
+            break
+        else:
+            for item in br.find_by_tag('p'):
+                if item.visible and any(k in item.text for k in error_text):
+                    print item.text
+                    return False
+        time.sleep(1)
     time.sleep(2)
-    down_button.click()
+    for i in range(60):    # timeout 2 min
+        if not "{0}.apk.part".format(package_name) in os.listdir(location):
+            break
+        time.sleep(2)
+    else:
+        print 'TIMEOUT: Download took more than 2 minutes'
+        return False
+    return True
 
 def get_signature(package_name, location):
     """
@@ -50,10 +71,10 @@ def main():
     LOCATION = str("C:\\SSD-Folders\\Google Drive (SSD)\\Projects\\AppDataGrabber\\Testing\\APKs")
     PACKAGE_NAME = raw_input('Enter package name: ')
 
-    download_apk(PACKAGE_NAME, LOCATION)
-    time.sleep(5)
-    print get_signature(PACKAGE_NAME, LOCATION)
-    print get_google_play_info(PACKAGE_NAME)
+    success = download_apk(PACKAGE_NAME, LOCATION)
+    if success:
+        print get_signature(PACKAGE_NAME, LOCATION)
+        print get_google_play_info(PACKAGE_NAME)
 
 if __name__ == "__main__":
     main()
