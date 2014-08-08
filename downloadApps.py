6b3b1d7e3e5fc69061dc8f67dee5a112013c7ede
jacobstein123 <jacobstein123@gmail.com>
2014-08-07 23:41:25 -0400

    added downloadApps.py
    Goes through all the apps in the database and downloads them if they
    have not been downloaded.

---
+++ downloadApps.py
@@ -0,0 +1,16 @@
+import time
+import clicker
+from getGooglePlayData import execute_command, execute_query
+
+db = {'host'  : 'localhost',
+      'user'  : 'root',
+      'passwd': '',
+      'db'    : 'mysql'}
+
+apps_to_download = execute_query(db,'select * from apps where apk_file is NULL;')
+for app in apps_to_download:
+    clicker.download_app(app['package_name'])
+    execute_command(db,'update apps set apk_file = "googleplay/{0}.apk";'.format(app['package_name']))
+    time.sleep(3)
+
+
